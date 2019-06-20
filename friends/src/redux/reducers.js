78b5ec8ec4a37b1a94422d66a561be91893279c4
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-20 14:37:59 +0100

    create initial reducer functions

--- friends/src/redux/reducers.js
+++ friends/src/redux/reducers.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import * as types from "./actionTypes";
 
 const initialState = {
   deletingFriend: false,
@@ -10,7 +11,55 @@ const initialState = {
   error: null
 };
 export const friends = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case (types.DELETE_FRIEND):
+      return state;
+    
+    case (types.DELETE_FRIEND_FAILURE):
+      return state;
+    
+    case (types.DELETE_FRIEND_SUCCESS):
+      return state;
+    
+    case (types.SAVE_FRIEND):
+      return state;
+
+    case (types.SAVE_FRIEND_SUCCESS):
+      return state;
+
+    case (types.SAVE_FRIEND_FAILURE):
+      return state;
+    
+    case (types.ADD_FRIENDS):
+      return state;
+    
+    case (types.ADD_FRIENDS_SUCCESS):
+      return state;
+
+    case (types.ADD_FRIENDS_FAILURE):
+      return state;
+
+    case (types.UPDATE_FRIEND):
+      return state;
+
+    case (types.UPDATE_FRIEND_SUCCESS):
+      return state;
+
+    case (types.UPDATE_FRIEND_FAILURE):
+      return state;
+    
+    case (types.LOGIN):
+      return state;
+    
+    case (types.LOGIN_SUCCESS):
+      return state;
+
+    case (types.LOGIN_FAILURE):
+      return state;
+  
+    default:
+      return state;
+  }
 };
 
 export default combineReducers({
