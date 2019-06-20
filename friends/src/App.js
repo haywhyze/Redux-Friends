3150a9679dd2878151ca041f9a2acdb84f58054c
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-20 15:59:48 +0100

    add routes and NavLinks to App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Link, Route } from 'react-router-dom';
+import {Login} from './components/Login';
+import Friends from './components/Friends';
 
 function App() {
   return (
-    <div>
-      Friends
-    </div>
+    <BrowserRouter>
+      <div>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/login'>Login</NavLink>
+        
+        <Route 
+          exact
+          path='/login'
+          component={Login}
+        />
+        <Friends />
+      </div>
+    </BrowserRouter>
   );
 }
 
