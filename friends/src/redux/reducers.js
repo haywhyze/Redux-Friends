844f285821937660b2f7656faee7c0ecd3ed11d0
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-20 15:02:57 +0100

    add reducers functionalities

--- friends/src/redux/reducers.js
+++ friends/src/redux/reducers.js
@@ -8,54 +8,55 @@ const initialState = {
   loggingIn: false,
   savingFriends: false,
   updatingFriend: false,
+  loggedIn: false,
   error: null
 };
 export const friends = (state = initialState, action) => {
   switch (action.type) {
     case (types.DELETE_FRIEND):
-      return state;
+      return { ...state, deletingFriend: true };
     
     case (types.DELETE_FRIEND_FAILURE):
-      return state;
+      return { ...state, error: action.payload, deletingFriend: false };
     
     case (types.DELETE_FRIEND_SUCCESS):
-      return state;
+      return { friends: state.friends.filter(friend => friend.id !== action.payload), deletingFriend: false};
     
     case (types.SAVE_FRIEND):
-      return state;
+      return { ...state, savingFriends: true };
 
     case (types.SAVE_FRIEND_SUCCESS):
-      return state;
+      return { ...state, friends: state.friends.concat(action.payload), savingFriends: false };
 
     case (types.SAVE_FRIEND_FAILURE):
-      return state;
+      return { ...state, error: action.payload, savingFriends: false };
     
     case (types.ADD_FRIENDS):
-      return state;
+      return { ...state, fetchingFriends: true };
     
     case (types.ADD_FRIENDS_SUCCESS):
-      return state;
+      return { ...state, friends: action.payload, fetchingFriends: false };
 
     case (types.ADD_FRIENDS_FAILURE):
-      return state;
+      return { ...state, error: action.payload, fetchingFriends: false };
 
     case (types.UPDATE_FRIEND):
-      return state;
+      return { ...state, updatingFriend: true };
 
     case (types.UPDATE_FRIEND_SUCCESS):
       return state;
 
     case (types.UPDATE_FRIEND_FAILURE):
-      return state;
+      return { ...state, error: action.payload, updatingFriend: false };;
     
     case (types.LOGIN):
-      return state;
+      return { ...state, loggingIn: true };
     
     case (types.LOGIN_SUCCESS):
-      return state;
+      return { ...state, loggedIn: true };
 
     case (types.LOGIN_FAILURE):
-      return state;
+      return { ...state, error: action.payload, loggingIn: false };
   
     default:
       return state;
