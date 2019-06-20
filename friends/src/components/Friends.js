9794bb3bbae04e01bdb41c0cb15ebc4fa57c79b4
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-20 19:45:34 +0100

    connect store and actionCreators to Friends.js

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,11 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { removeFriend, saveFriend, fetchFriends, addOneFriend } from "../redux/actionCreators";
 
-export default class Friends extends Component {
+export class Friends extends Component {
+
+  componentDidMount() {
+    this.props.fetchFriends();
+  }
   render() {
+    const { friends } = this.props;
     return (
       <div>
-        Welcome
+        {
+          friends.map(friend => <p key={friend.id}>{friend.name}</p>)
+        }
       </div>
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    deletingFriend: state.friends.deletingFriend,
+    fetchingFriends: state.friends.fetchingFriends,
+    friends: state.friends.friends,
+    savingFriends: state.friends.savingFriends,
+    updatingFriend: state.friends.updatingFriend,
+    error: state.friends.error
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  {
+    removeFriend,
+    saveFriend,
+    fetchFriends,
+    addOneFriend
+  }
+)(Friends);
