8d353b0c344f716eb54e16922b0c4d785d4218ce
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-21 07:05:08 +0100

    add SignOut action Type

--- friends/src/redux/actionTypes.js
+++ friends/src/redux/actionTypes.js
@@ -13,3 +13,4 @@ export const UPDATE_FRIEND_SUCCESS = 'UPDATE_FRIEND_SUCCESS';
 export const LOGIN = 'LOGIN';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const SIGNOUT = 'SIGNOUT';
